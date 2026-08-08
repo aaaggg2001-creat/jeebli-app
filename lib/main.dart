ef7a3b5d4c7f5ff7344e9dae22d9e84dc2b46084
aaaggg2001-creat <aaaggg2001@github.com>
2026-08-09 02:31:25 +0300

    Fix missing firebase_auth import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
